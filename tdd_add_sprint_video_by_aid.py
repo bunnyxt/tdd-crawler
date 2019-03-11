5fedc2321f33ec7f85e175af3431fcb424bd71e8
bunnyxt <bunnyxt@outlook.com>
2019-03-12 01:10:27 +0800

    Add tdd add sprint video by aid script.

---
+++ tdd_add_sprint_video_by_aid.py
@@ -0,0 +1,13 @@
+"""
+tdd add focus video by aid
+"""
+import sys
+from biliapi import TddAddSprintVideo
+from db import Session
+
+if __name__ == '__main__':
+    if len(sys.argv) < 1:
+        print('error: need more args.')
+    else:
+        aid = sys.argv[1]
+        TddAddSprintVideo.add_sprint_video(aid, Session())
